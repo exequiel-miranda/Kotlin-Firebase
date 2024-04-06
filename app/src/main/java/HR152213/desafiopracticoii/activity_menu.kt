da20d7c89294e6b50fe6e2b829c1d9652ab02a53
Bryan <bryan_miranda@ricaldone.edu.sv>
2024-04-06 17:57:08 -0600

    final version

--- app/src/main/java/HR152213/desafiopracticoii/activity_menu.kt
+++ app/src/main/java/HR152213/desafiopracticoii/activity_menu.kt
@@ -3,6 +3,7 @@ package HR152213.desafiopracticoii
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,7 @@ class activity_menu : AppCompatActivity() {
 
         val btnAgregarTicket = findViewById<Button>(R.id.btnAgregarTicket)
         val btnVerListado = findViewById<Button>(R.id.btnVerListado)
+        val txtCerrarSesion = findViewById<TextView>(R.id.txtCerrarSesion)
 
         btnAgregarTicket.setOnClickListener {
             val activity_tickets = Intent(this, activity_tickets::class.java)
@@ -24,6 +26,11 @@ class activity_menu : AppCompatActivity() {
         btnVerListado.setOnClickListener {
             val activity_listado_tickets = Intent(this, activity_listado_tickets::class.java)
             startActivity(activity_listado_tickets)
+        }
+
+        txtCerrarSesion.setOnClickListener {
+            val activity_main = Intent(this, MainActivity::class.java)
+            startActivity(activity_main)
         }
     }
 }
